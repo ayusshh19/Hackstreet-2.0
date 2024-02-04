39e9686b988fc3a5076c8b7b8adf1e9c793dee9e
ayusshh19 <cm.b.49ayush.shukla@gmail.com>
2024-02-04 11:59:48 +0530

    format bug

--- src/action/Chataction.js
+++ src/action/Chataction.js
@@ -21,7 +21,7 @@ function format(data) {
   const codeRegex = /```([\s\S]*?)```/g;
   const formattedResponse = res.replace(
     codeRegex,
-    (_, code) => `<pre className="p-4 b-2">${code}</pre>`
+    (_, code) => `<pre className="p-4 border-2">${code}</pre>`
   );
   const replacedText = formattedResponse.replace(
     /\*\*(.*?)\*\*/g,
